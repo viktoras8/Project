3ebeb853e47deb65a9b2ddbc182a090ba9e56e69
viktoras8 <118976000+viktoras8@users.noreply.github.com>
2024-04-17 02:36:54 -0900

    Add files via upload

---
+++ Biblioteka/library_Project/otheruser_dashboard.php
@@ -0,0 +1,237 @@
+<?php
+session_start();
+$userloginid=$_SESSION["userid"] = $_GET['userlogid'];
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Admin Dashboard</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+      <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <!-- <link rel="stylesheet" href="style.css"> -->
+    </head>
+    <style>
+body{
+background-color: #fbffb0;
+}
+.innerright,label {
+    color: rgb(16, 170, 16);
+    font-weight:bold; 
+}
+.container,
+.row,
+.imglogo {
+    margin:auto;
+}
+.imglogo{
+    width: 400px;
+    height: 200px;
+    margin-right: 210px;
+}
+
+input{
+    margin-left:20px;
+}
+.leftinnerdiv {
+    float: left;
+    width: 25%;
+}
+
+.rightinnerdiv {
+    float: right;
+    width: 75%;
+}
+
+.innerright {
+    background-color: #2768e7;
+    margin-bottom: 100px;
+}
+
+.greenbtn {
+    background-color: #2768e7;
+    color: black;
+    width: 100%;
+    height: 40px;
+    margin-top: 8px;
+    border-color: black;
+}
+
+.greenbtn,
+a {
+    text-decoration: none;
+    color: black;
+    font-size: large;
+}
+
+
+td{
+    background-color: white;
+    color: black;
+}
+td, a{
+    color:black;
+}
+    </style>
+    <body>
+
+    <?php
+   include("data_class.php");
+    ?>
+           <div class="container">
+            <div class="innerdiv">
+            <div class="row"><img class="imglogo" src="images/logo_bg.png"/></div>
+            <div class="leftinnerdiv">
+                <br>
+                <Button class="greenbtn" onclick="openpart('myaccount')"> <img class="icons" src="images/icon/profile.png" width="30px" height="30px"/>  MY ACCOUNT</Button>
+                <Button class="greenbtn" onclick="openpart('requestbook')"><img class="icons" src="images/icon/book.png" width="30px" height="30px"/> REQUEST BOOK</Button>
+                <Button class="greenbtn" onclick="openpart('issuereport')"> <img class="icons" src="images/icon/monitoring.png" width="30px" height="30px"/>  BOOK RECORD</Button>
+                <a href="index.php"><Button class="greenbtn" ><img class="icons" src="images/icon/logout.png" width="30px" height="30px"/> LOGOUT</Button></a>
+            </div>
+
+
+            <div class="rightinnerdiv">   
+            <div id="myaccount" class="innerright portion" style="<?php  if(!empty($_REQUEST['returnid'])){ echo "display:none";} else {echo ""; }?>">
+            <Button class="greenbtn" >My Account</Button>
+
+            <?php
+
+            $u=new data;
+            $u->setconnection();
+            $u->userdetail($userloginid);
+            $recordset=$u->userdetail($userloginid);
+            foreach($recordset as $row){
+
+            $id= $row[0];
+            $name= $row[1];
+            $email= $row[2];
+            $pass= $row[3];
+            $type= $row[4];
+            }               
+                ?>
+
+            <p style="color:black"><u>Person Name:</u> &nbsp&nbsp<?php echo $name ?></p>
+            <p style="color:black"><u>Person Email:</u> &nbsp&nbsp<?php echo $email ?></p>
+            <p style="color:black"><u>Account Type:</u> &nbsp&nbsp<?php echo $type ?></p>
+        
+            </div>
+            </div>
+
+
+            
+
+
+
+            <div class="rightinnerdiv">   
+            <div id="issuereport" class="innerright portion" style="<?php  if(!empty($_REQUEST['returnid'])){ echo "display:none";} else {echo "display:none"; }?>">
+            <Button class="greenbtn" >BOOK RECORD</Button>
+
+            <?php
+
+            $userloginid=$_SESSION["userid"] = $_GET['userlogid'];
+            $u=new data;
+            $u->setconnection();
+            $u->getissuebook($userloginid);
+            $recordset=$u->getissuebook($userloginid);
+
+            $table="<table style='font-family: Arial, Helvetica, sans-serif;border-collapse: collapse;width: 100%;color: #453e20;'><tr><th style='  
+            padding: 8px;'>Name</th><th>Book Name</th><th>Issue Date</th><th>Return Date</th><th>Fine</th></th><th>Return</th></tr>";
+
+            foreach($recordset as $row){
+                $table.="<tr>";
+               "<td>$row[0]</td>";
+                $table.="<td>$row[2]</td>";
+                $table.="<td>$row[3]</td>";
+                $table.="<td>$row[6]</td>";
+                $table.="<td>$row[7]</td>";
+                $table.="<td>$row[8]</td>";
+                $table.="<td><a href='otheruser_dashboard.php?returnid=$row[0]&userlogid=$userloginid'><button type='button' class='btn btn-primary'>Return</button></a></td>";
+                $table.="</tr>";
+                // $table.=$row[0];
+            }
+            $table.="</table>";
+
+            echo $table;
+            ?>
+
+            </div>
+            </div>
+
+
+            <div class="rightinnerdiv">   
+            <div id="return" class="innerright portion" style="<?php  if(!empty($_REQUEST['returnid'])){ $returnid=$_REQUEST['returnid'];} else {echo "display:none"; }?>">
+            <Button class="greenbtn" >Return Book</Button>
+
+            <?php
+
+            $u=new data;
+            $u->setconnection();
+            $u->returnbook($returnid);
+            $recordset=$u->returnbook($returnid);
+                ?>
+
+            </div>
+            </div>
+
+
+            <div class="rightinnerdiv">   
+            <div id="requestbook" class="innerright portion" style="<?php  if(!empty($_REQUEST['returnid'])){ $returnid=$_REQUEST['returnid'];echo "display:none";} else {echo "display:none"; }?>">
+            <Button class="greenbtn" >REQUEST BOOK</Button>
+
+            <?php
+            $u=new data;
+            $u->setconnection();
+            $u->getbookissue();
+            $recordset=$u->getbookissue();
+
+            $table="<table style='font-family: Arial, Helvetica, sans-serif;border-collapse: collapse;width: 100%; color: #453e20;'><tr>
+            <th>Image</th><th>Book Name</th><th>Book Authour</th><th>branch</th><th>price</th></th><th>Request Book</th></tr>";
+
+            foreach($recordset as $row){
+                $table.="<tr>";
+               "<td>$row[0]</td>";
+               $table.="<td><img src='uploads/$row[1]' width='100px' height='100px' style='border:1px solid #333333;'></td>";
+               $table.="<td>$row[2]</td>";
+                $table.="<td>$row[4]</td>";
+                $table.="<td>$row[6]</td>";
+                $table.="<td>$row[7]</td>";
+                $table.="<td><a href='requestbook.php?bookid=$row[0]&userid=$userloginid'><button type='button' class='btn btn-primary'>Request Book</button></a></td>";
+           
+                $table.="</tr>";
+                // $table.=$row[0];
+            }
+            $table.="</table>";
+
+            echo $table;
+
+
+                ?>
+
+            </div>
+            </div>
+
+        </div>
+        </div>
+
+
+        <script>
+        function openpart(portion) {
+        var i;
+        var x = document.getElementsByClassName("portion");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        document.getElementById(portion).style.display = "block";  
+        }
+
+   
+ 
+        
+        </script>
+    </body>
+</html>
